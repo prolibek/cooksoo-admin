8af86957c0b880717024dec728c6a47c9dcd784b
Alibek Baigulov <baigulov_a@au.kg>
2023-11-17 12:29:08 +0600

    set up everything

---
+++ backend/users/views.py
@@ -0,0 +1,14 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        pass
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        pass 
+
+class LogoutAPIView(APIView):
+    def post(self, request):
+        pass
